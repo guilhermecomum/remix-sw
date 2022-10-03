3b3a45e8cd00a90ae5903b0eac7427c7efca5d0f
Guilherme Guerra <guilherme.ga@gmail.com>
2022-10-03 09:51:22 -0300

    Fixes tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,48 @@
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        star: {
+          '0%': {
+            opacity: 0,
+            transform: 'scale(1.5) translateY(-0.75em)',
+          },
+          '20%': {
+            opacity: 1,
+          },
+          '89%': {
+            opacity: 1,
+            transform: 'scale(1)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translateZ(-1000em)',
+          },
+        },
+        wars: {
+          '0%': {
+            opacity: 0,
+            transform: 'scale(1.5) translateY(0.5em)',
+          },
+          '20%': {
+            opacity: 1,
+          },
+          '90%': {
+            opacity: 1,
+            transform: 'scale(1)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translateZ(-1000em)',
+          },
+        },
+      },
+      animation: {
+        'opening-wars': 'wars 5s ease-out',
+        'opening-star': 'star 5s ease-out',
+      },
+    },
   },
   plugins: [],
 }
