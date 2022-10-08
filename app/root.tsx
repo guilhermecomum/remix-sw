987a19027c7745a1055d4549936d6ca4e9581259
Guilherme Guerra <guilherme.ga@gmail.com>
2022-10-08 11:08:56 -0300

    Update page title

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,7 @@ export function links() {
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'Remix Star Wars',
   viewport: 'width=device-width,initial-scale=1',
 })
 
